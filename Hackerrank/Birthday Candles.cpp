6d6cf1ce03f8d6a6985bdecf087f832ecb92afc8
Ekansh MAngal <ekanshmangal102@gmail.com>
2020-06-12 00:48:16 +0530

    Birthday Candle hackerrank problem solved

--- Hackerrank/Birthday Candles.cpp
+++ Hackerrank/Birthday Candles.cpp
@@ -1,41 +1,88 @@
-#include<iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the birthdayCakeCandles function below.
+int birthdayCakeCandles(vector<int> ar) {
+    
+    sort(ar.begin(),ar.end(),greater<int>());
+    int a=ar[0];
+    vector<int>::iterator it;
+    // map<int,int>::iterator mp;
+    int count=0;
+    int n=ar.size();
+    for(int i=0;i<n;i++)
+    {
+        if(ar[i]==a)
+        {
+            count++;
+        }
+       
+       else
+	   {
+			return count;
+	   }	
+    }
+    return count;
+}
 
 int main()
 {
-	//Declarations
-	int n; 
-	long long int a[100], temp=0;
-	int i=0, j=0, count=0;
-	
-	//cout<<"\nEnter size of array: \n";
-	cin>>n;
-	
-	//cout<<"\nEnter Elements in array: \n";
-	for(i=0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	
-	//Sorting
-	for(i=0; i<n; i++)
-	{
-		for(j=0; j<n-1; j++)
-		{
-			if(a[j] > a[j+1])
-			{
-				temp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = temp;
-			}
-		}
-	}
-	
-	for(i=0;i<n;i++)
-	{
-		if(a[n-1] == a[i])
-			count++;
-	}	
-	
-	cout<<count;
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int ar_count;
+    cin >> ar_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split_string(ar_temp_temp);
+
+    vector<int> ar(ar_count);
+
+    for (int i = 0; i < ar_count; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = birthdayCakeCandles(ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
 }
